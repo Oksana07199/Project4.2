e88055d818ec052cc412ae3c89fc1895c1c15528
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-28 20:03:14 +0300

    okruglenie

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public double index(double weight, double growth) {
         double resalt = weight / (growth * growth);
-
         return resalt;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService calculate = new BmiService();
-        double indexMass = calculate.index(98, 187);
-        System.out.println(indexMass);
+        double indexMass= calculate.index(98, 1.87);
+        System.out.println ((int)indexMass);
 
     }
 }
